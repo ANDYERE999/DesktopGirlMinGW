7423bbca90a810ab9ed9f174b62fd893abc7bbf0
wym <johndoe@example.com>
2025-05-28 15:26:36 +0800

    成功基于QT6+Win10_x86_64+CMake+MinGW1310+CLion移植了Live2D官方SDK

---
+++ build/CMakeFiles/CMakeScratch/TryCompile-vngqaj/CMakeFiles/cmTC_bd6a2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_bd6a2.dir/src.cxx.obj"
+  "cmTC_bd6a2.exe"
+  "cmTC_bd6a2.exe.manifest"
+  "cmTC_bd6a2.pdb"
+  "libcmTC_bd6a2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmTC_bd6a2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
